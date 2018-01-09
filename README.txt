d8ec9d9405b165919c6d236df9c759fa5b312af3
SecurityGuyJW <35028309+SecurityGuyJW@users.noreply.github.com>
2018-01-09 15:12:57 -0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,1 @@
+netcat duplicate python style.
---
+++ pycat.py
@@ -0,0 +1,49 @@
+#************************************************************************************
+#*									  	                                            *
+#*		This is a ncat duplicate made from python programming language		        *
+#*										                                            *
+#*										                                            *
+#************************************************************************************
+
+
+import sh
+
+
+def getUserInput():
+    var = input(">")
+    return var
+
+def userInstruction():
+    print("hi")
+
+def userExecution(x):
+
+    if x == 'n':
+        print(x)
+    if x == 'n':
+        print(x)
+    if x == 'n':
+        print(x)
+    if x == 'n':
+        print(x)
+    if x == 'n':
+        print(x)
+    if x == 'n':
+        print(x)
+    if x == 'n':
+        print(x)
+    if x == 'n':
+        print(x)
+
+def userOutput():
+    print("result")
+
+def main():
+
+    var = getUserInput()
+
+    userExecution(var)
+
+main()
+
+
